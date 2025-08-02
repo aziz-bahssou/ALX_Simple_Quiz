e07fa60b024f3b515caba03acebdbb3a02f4fd9d
Aziz <azizbahssou444@gmail.com>
2025-08-02 19:39:57 +0100

    .

--- quiz.js
+++ quiz.js
@@ -1,17 +1,23 @@
+
 function checkAnswer() {
+    
     const correctAnswer = "4";
+
+
     const selectedOption = document.querySelector("input[name='quiz']:checked");
-    const userAnswer = selectedOption ? selectedOption.value: null;
+    const userAnswer = selectedOption ? selectedOption.value : null;
+
+   
     const feedback = document.getElementById("feedback");
 
-    if (userAnswer === correctAnswer){
+    
+    if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
-        feedback.style.color= 'green';
-    }else {
-        feedback.textContent = "thar's incorrect. Try again!"
-        feedback.style.color= 'red'
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
     }
 }
+
+
 const submitButton = document.getElementById("submit-answer");
 submitButton.addEventListener("click", checkAnswer);
-
